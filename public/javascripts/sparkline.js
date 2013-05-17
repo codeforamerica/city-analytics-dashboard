d0783f087b5805b6f9df5927c8458aa0749234ec
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-05-17 17:47:38 +0100

    sparkline: dedupe scale creation

--- public/javascripts/sparkline.js
+++ public/javascripts/sparkline.js
@@ -1,8 +1,22 @@
 (function(){
-  "use strict"
+  "use strict";
   var root = this,
       $ = root.jQuery;
-  if(typeof root.matrix === 'undefined'){ root.matrix = {} }
+
+  if(typeof root.matrix === 'undefined'){ root.matrix = {}; }
+
+  var makeScales = function(data, width, height) {
+    var maxY = d3.max(data),
+        minY = d3.min(data);
+
+    maxY = maxY + (maxY * 0.01);
+    minY = minY - (minY * 0.01);
+
+    var x = d3.scale.linear().domain([0, data.length-1]).range([-(width/data.length), width+(width/data.length)]),
+        y = d3.scale.linear().domain([minY, maxY]).range([height, 0]);
+
+    return {x: x, y: y};
+  };
 
   var sparkline = function(el, options){
     var width = options.width || 200,
@@ -13,41 +27,29 @@
             .attr('width', width)
             .attr('height', height)
             .attr('class', 'sparkline'),
-        maxY = d3.max(data),
-        maxY = maxY + (maxY * 0.01),
-        minY = d3.min(data),
-        minY = minY - (minY * 0.01),
-        x = d3.scale.linear().domain([0, data.length-1]).range([-(width/data.length), width+(width/data.length)]),
-        y = d3.scale.linear().domain([minY, maxY]).range([height, 0]), // flipped so zero is along the bottom
+        scale = makeScales(data, width, height),
         line = d3.svg.line()
           .interpolate('basis')
-          .x(function(d, i) { return x(i); })
-          .y(function(d, i) { return y(d); }),
+          .x(function(d, i) { return scale.x(i); })
+          .y(function(d, i) { return scale.y(d); }),
         path = svg.append('svg:path')
           .data([data])
           .attr('d', line);
 
     return {
       update: function(newData){
-        var maxY = d3.max(newData),
-            minY = d3.min(newData);
-        maxY = maxY + (maxY * 0.01);
-        minY = minY - (minY * 0.01);
-
-        x = d3.scale.linear().domain([0, newData.length-1]).range([-(width/newData.length), width+(width/newData.length)]),
-        y = d3.scale.linear().domain([minY, maxY]).range([height, 0]);
-
+        scale = makeScales(newData, width, height);
         svg.selectAll('path')
         .data([newData])
-          .attr("transform", "translate(" + x(1) + ")")
+          .attr("transform", "translate(" + scale.x(1) + ")")
           .attr('d', line)
           .transition()
           .duration(500)
           .ease('linear')
-          .attr("transform", "translate(" + x(0) + ")");
+          .attr("transform", "translate(" + scale.x(0) + ")");
       }
-    }
-  }
+    };
+  };
 
   root.matrix.sparkline = sparkline;
 }).call(this);
