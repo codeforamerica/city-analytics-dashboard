c6a474ed9151a01dd6c7b389b1f0cab353351fea
Frances Berriman <fberriman@gmail.com>
2014-04-23 15:43:12 -0700

    Update query for top content

--- public/javascripts/content.js
+++ public/javascripts/content.js
@@ -11,19 +11,16 @@
     /* this is GDS specific. It's pulling from their API */
     endpoint: function(){
       //return "https://www.performance.service.gov.uk/data/govuk/trending?limit=10&sort_by=percent_change:descending";
-      return "/realtime?ids=ga%3A41226190&metrics=rt%3Apageviews&dimensions=rt%3ApageTitle&max-results=50&sort=-rt%3Apageviews"
+      return "/realtime?ids=ga%3A41226190&metrics=rt%3Apageviews&dimensions=rt%3ApageTitle&max-results=20&sort=-rt%3Apageviews"
     },
     parseResponse: function(data){
-      console.log(data.rows.length);
-     // console.log(data.rows[0][0])
-     // console.log(data.rows[0])
       var i, _i;
 
       content.pages = [];
       for(i=0,_i=data.rows.length; i<_i; i++){
-        //console.log(data.rows[i][0])
+        console.log(data.rows[i][0])
         content.pages.push({
-          title: data.rows[i][0].split(' — ').slice(0,-1).join(' - '),
+          title: data.rows[i][0]//.split(' — ').slice(0,-1).join(' - '),
           //displayHits: root.matrix.numberWithCommas(data.rows[i].week2),
           //percentageUp: root.matrix.numberWithCommas(Math.round(data.rows[i].percent_change)) + "%"
         });
@@ -32,7 +29,7 @@
       content.displayResults();
     },
     displayResults: function(){
-      matrix.template(content.$el, 'content-results', { pages: content.pages.slice(0,10) });
+      matrix.template(content.$el, 'content-results', { pages: content.pages.slice(0,20) });
     },
     init: function(){
       content.$el = $('#content');
