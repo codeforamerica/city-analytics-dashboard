f8ef7e78a2c39aeb4f11d161bf277d2775594051
Frances Berriman <fberriman@gmail.com>
2014-06-23 14:56:53 -0700

    Top pages now click through to the URL provided

--- public/javascripts/content.js
+++ public/javascripts/content.js
@@ -8,7 +8,7 @@
     el: false,
 
     endpoint: function(){
-      return "/realtime?ids=ga:"+matrix.settings.profileId+"&metrics=rt%3Apageviews&dimensions=rt%3ApageTitle&max-results=10&sort=-rt%3Apageviews"
+      return "/realtime?ids=ga:"+matrix.settings.profileId+"&metrics=rt%3Apageviews&dimensions=rt%3ApageTitle,rt:pagePath&max-results=10&sort=-rt%3Apageviews"
     },
     parseResponse: function(data){
       var i, _i;
@@ -17,7 +17,8 @@
       for(i=0,_i=data.rows.length; i<_i; i++){
         content.pages.push({
           title: data.rows[i][0],//.split(' — ').slice(0,-1).join(' - '),
-          visits: data.rows[i][1]
+          url: data.rows[i][1],
+          visits: data.rows[i][2]
           //displayHits: root.matrix.numberWithCommas(data.rows[i].week2),
           //percentageUp: root.matrix.numberWithCommas(Math.round(data.rows[i].percent_change)) + "%"
         });
