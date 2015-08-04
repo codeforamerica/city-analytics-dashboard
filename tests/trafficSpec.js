6cae911047d15eef3bb48c39a448f8b66f54ee49
Mila Frerichs <mila.frerichs@gmail.com>
2015-08-04 14:03:56 +0200

    Add test for the endpoint method

--- tests/trafficSpec.js
+++ tests/trafficSpec.js
@@ -14,4 +14,19 @@ describe('traffic', function() {
   it('has interval of 2 minutes', function() {
     expect(subject.interval).to.eq(120000);
   });
+  describe('#endpoint', function() {
+    it('returns the path to the servers realtime endpoint', function() {
+      expect(subject.endpoint()).to.eql('/realtime?ids=ga:&metrics=rt:activeUsers&max-results=10');
+    });
+    context('with profileId', function() {
+      beforeEach(function() {
+        window.matrix.settings = {
+          profileId: 'Test'
+        };
+      });
+      it('returns correct profile Id in the endpoint path', function() {
+        expect(subject.endpoint()).to.eql('/realtime?ids=ga:Test&metrics=rt:activeUsers&max-results=10');
+      });
+    });
+  });
 });
