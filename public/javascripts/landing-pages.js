b4d4a1b97ae293e7f440d4075acf420c201322c7
Mila Frerichs <mila.frerichs@gmail.com>
2015-08-18 09:25:00 +0200

    Update active pages to always show the active pages
    add handlebars.js for template generation to replace mustache
    handlebars has much better support for older browsers and is way faster
    
    replace the population of new entries; replace all entries with new ones
    from the API. since they are active after all no need to be too clever
    about the population of the array.
    show all new entries at once and use css animations to transition the
    new entries
    replace js animation which depend on Chrome

--- public/javascripts/landing-pages.js
+++ public/javascripts/landing-pages.js
@@ -5,13 +5,7 @@
 
   var landing = {
     terms: [],
-    urls: [],
-    newTerms: [],
-    newURLs: [],
-    newSources: [],
     el: false,
-    nextRefresh: 0,
-    limit: 10,
 
     endpoint: function(){
       return "/realtime?"
@@ -42,31 +36,17 @@
       return true;
     },
     addTerm: function(term, count, url, source){
-
-      var i, _i;
-      for(i=0, _i=landing.terms.length; i<_i;  i++){
-        if(landing.terms[i].term === term){
-          landing.terms[i].nextTick = count;
-          return true;
-        }
-      }
       landing.terms.push({
         term: term,
-        total: 0,
-        nextTick: count,
-        currentTick: 0,
+        total: count,
         url: url,
-        source: source
+        source: source,
+        has_url: !!url,
+        has_source: !!source && (source != "(not set)"),
+        multiple_visits: (count > 1)
       });
     },
-    zeroNextTicks: function(){
-      var i, _i, newTerms = [];
-      for(i=0, _i=landing.terms.length; i<_i;  i++){
-        landing.terms[i].nextTick = 0;
-      }
-    },
-    addNextTickValues: function(data){
-
+    parseData: function(data) {
       var i, _i, term, url, source;
       for(i=0,_i=data.rows.length; i<_i; i++){
         term = data.rows[i][0].split(' - ');
@@ -75,74 +55,32 @@
         if(term[0] !== 'Search' && landing.safeTerm(term[0])){
           landing.addTerm(term[0], root.parseInt(data.rows[i][3], 10), url, source);
         }
-
       }
     },
-    addTimeIndexValues: function(){
-      var i, _i, j, _j, term, time, newPeople, nonZeroTerms = [];
-      for(i=0, _i=landing.terms.length; i<_i;  i++){
-        term = landing.terms[i];
-        newPeople = term.currentTick < term.nextTick ? term.nextTick - term.currentTick : 0;
-        term.total = term.total + newPeople;
-        term.currentTick = term.nextTick;
-        if(newPeople > 0){
-          for(j=0,_j=newPeople; j<_j; j++){
-            landing.newTerms.push(term.term);
-            landing.newURLs.push(term.url);
-            landing.newSources.push(term.source);
-          }
-        }
-        if(term.currentTick > 0){
-          nonZeroTerms.push(term);
-        }
+    parseResponse: function(error, data){
+      if(error) { return -1; }
+      if(data.hasOwnProperty('rows')) {
+        landing.terms = [];
+        landing.parseData(data);
+        landing.refreshResults();
+      }else {
+        return -1;
       }
-      //landing.newTerms.sort(function(){ return Math.floor((Math.random() * 3) - 1) });
-      landing.terms = nonZeroTerms;
     },
-    parseResponse: function(data){
-      var term, i, _i;
-
-      landing.zeroNextTicks();
-      landing.addNextTickValues(data);
-      landing.addTimeIndexValues();
-    },
-    displayResults: function(){
-      var term = landing.newTerms.pop();
-      var url = landing.newURLs.pop();
-      var source = landing.newSources.pop();
-      var el = landing.el;
-      if(term){
-        var context = {
-          term: term,
-          has_url: !!url,
-          url: url,
-          has_source: !!source && (source != "(not set)"),
-          source: source
-        }
-
-        var tempList = el.ol().template("landing-pages-item", context);
-        root.matrix.manager.animateInto(tempList.firstElementChild, el,
-                                        landing.limit);
-        setTimeout(landing.displayResults,
-                  (landing.nextRefresh - Date.now())/landing.newTerms.length);
-      } else {
-        setTimeout(landing.displayResults, 5000);
-      }
+    refreshResults: function() {
+      var html    = landing.compiledTemplate({pages: landing.terms });
+      landing.el.innerHTML = html + landing.el.innerHTML;
     },
     init: function(){
+      var source;
       landing.el = document.getElementById('search');
+      source = document.getElementById("landing-pages-items");
+      landing.compiledTemplate = Handlebars.compile(source.innerHTML);
       landing.reload();
-      landing.displayResults();
       window.setInterval(landing.reload, 60e3);
-      document.addEventListener(
-          "visibilitychange",
-          function() { root.matrix.manager.positionFixups(landing); },
-          false);
     },
     reload: function(){
-      var endpoint = landing.endpoint();
-      landing.nextRefresh = Date.now() + 60e3;
-      d3.json(endpoint, landing.parseResponse);
+      d3.json(landing.endpoint(), landing.parseResponse);
     }
   };
 
--- tests/landingPagesSpec.js
+++ tests/landingPagesSpec.js
@@ -11,6 +11,122 @@ describe('traffic', function() {
     sandbox.restore();
     server.restore();
   });
+  describe('#init', function() {
+    beforeEach(function() {
+      $('body').append('<div id="landing-pages-items"></div>');
+      sandbox.stub(Handlebars, 'compile');
+    });
+    it("calls reload", function() {
+      mock = sandbox.mock(subject).expects("reload").once();
+      subject.init();
+      mock.verify();
+    });
+    it("calls reload with interval of 1 minute", function() {
+      clock = sinon.useFakeTimers(Date.now());
+      mock = sandbox.mock(subject).expects("reload").twice();
+      subject.init();
+      clock.tick(60000);
+      mock.verify();
+      clock.restore();
+    });
+  });
+  describe('#reload', function() {
+    it("calls endpoint", function() {
+      sandbox.stub(d3, "json");
+      mock = sandbox.mock(subject).expects("endpoint").once();
+      subject.reload();
+      mock.verify();
+    });
+    context('json returned', function(){
+      beforeEach(function() {
+        stub = sandbox.stub(d3, 'json');
+      });
+      it("calls parseResponse", function() {
+        mock = sandbox.mock(subject).expects("parseResponse").once();
+        subject.reload();
+        stub.callArgWith(1, {}, {});
+        mock.verify();
+      });
+    });
+  });
+  describe('#parseResponse', function() {
+    context("error parsing json", function() {
+      it("returns withpout parsing", function() {
+        mock = sandbox.mock(subject).expects("parseData").never();
+        subject.parseResponse({}, null);
+        mock.verify();
+      });
+      it("does not refresh the results", function() {
+        mock = sandbox.mock(subject).expects("refreshResults").never();
+        subject.parseResponse({}, null);
+        mock.verify();
+      });
+    });
+    context("no error parsing json", function() {
+      context("has data from GA", function() {
+        it("calls parseData", function() {
+          sandbox.stub(subject, 'refreshResults');
+          mock = sandbox.mock(subject).expects("parseData").once();
+          subject.parseResponse(null, {rows: []});
+          mock.verify();
+        });
+        it("refreshes the results", function() {
+          mock = sandbox.mock(subject).expects("refreshResults").once();
+          subject.parseResponse(null, {rows: []});
+          mock.verify();
+        });
+      });
+      context("no rows (no data from GA)", function() {
+        it("does not call parseData", function() {
+          mock = sandbox.mock(subject).expects("parseData").never();
+          subject.parseResponse(null, {});
+          mock.verify();
+        });
+      });
+    });
+  });
+  describe('#parseData', function() {
+    beforeEach(function() {
+      data = { rows: [["Term", "url", "source", "10"]]};
+    });
+    it("calls addTerm for every row", function() {
+      mock = sandbox.mock(subject).expects("addTerm").once().withArgs("Term", 10, "url", "source");
+      subject.parseData(data);
+      mock.verify();
+    });
+    context('Search Term', function() {
+      beforeEach(function(){
+        data = { rows: [["Search", "url", "source", "10"]]};
+      });
+      it("does not call addTerm for the row", function() {
+        mock = sandbox.mock(subject).expects("addTerm").never();
+        subject.parseData(data);
+        mock.verify();
+      });
+    });
+  });
+  describe('#addTerm', function() {
+    beforeEach(function() {
+      subject.terms = [];
+    });
+    it("adds new items to terms", function() {
+      expect(subject.addTerm).to.increase(subject.terms,'length');
+    });
+    it("adds new items to terms", function() {
+      result = { term: 'Test', total: 1, url: 'url', source: 'source', has_url: true, has_source: true, multiple_visits: false }
+      subject.addTerm("Test", 1, "url", "source");
+      expect(subject.terms[0]).to.eql(result);
+    });
+  });
+  describe('#refreshResults', function() {
+    beforeEach(function() {
+      subject.terms = [{ term: 'Test', total: 1, url: 'url', source: 'source' }];
+    });
+    it("shows all terms", function() {
+    });
+    it("calls handlebars template", function() {
+    });
+  });
   describe('#endpoint', function() {
     it('returns the path to the servers realtime endpoint', function() {
       expect(subject.endpoint()).to.eql('/realtime?ids=ga:&metrics=ga:activeVisitors&dimensions=ga:pageTitle,ga:pagePath,rt:source&sort=-ga:activeVisitors&max-results=10000');
