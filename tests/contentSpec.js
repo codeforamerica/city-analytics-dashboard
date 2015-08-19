e2eaf48c0cc9198a388e42d8c2ca5aa446df409e
Mila Frerichs <mila.frerichs@gmail.com>
2015-08-19 11:38:42 +0200

    Add tests for init method

--- tests/contentSpec.js
+++ tests/contentSpec.js
@@ -11,8 +11,23 @@ describe('traffic', function() {
     sandbox.restore();
     server.restore();
   });
-  it('has initial points', function() {
+  it('empty pages', function() {
     expect(subject.pages).to.eql([]);
+  });
+  describe('#init', function() {
+    it("calls reload", function() {
+      mock = sandbox.mock(subject).expects("reload").once();
+      subject.init();
+      mock.verify();
+    });
+    it("calls reload with interval of 30 minute", function() {
+      clock = sinon.useFakeTimers(Date.now());
+      mock = sandbox.mock(subject).expects("reload").twice();
+      subject.init();
+      clock.tick(1800000);
+      mock.verify();
+      clock.restore();
+    });
   });
   describe('#endpoint', function() {
     it('returns the path to the servers realtime endpoint', function() {
