918e720e8cfa48abf69c8e99e62a20e3d68e8fb2
Mila Frerichs <mila.frerichs@gmail.com>
2015-08-25 14:33:26 +0200

    Change order of sinon loading, downgrade jquery version
    could not get jquery to work on IE11 for these tests
    jquery 1.11. seems to work fine. need more evaluation

--- karma.saucelabs.js
+++ karma.saucelabs.js
@@ -20,7 +20,7 @@ module.exports = function(config) {
   };
   config.set({
     basePath: '',
-    frameworks: ['mocha', 'chai-jquery', 'jquery-2.1.0', 'chai', 'sinon-chai', 'fixture'],
+    frameworks: ['mocha', 'chai-jquery', 'jquery-1.11.0', 'sinon-chai', 'chai', 'fixture'],
     files: [
       'public/javascripts/vendor/d3.v3.min.js',
       'public/javascripts/vendor/raphael-min.js',
