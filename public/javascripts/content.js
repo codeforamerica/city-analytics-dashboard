4e5b58059d3c3fc54a9a4549e5e598c0cacf3956
Mila Frerichs <mila.frerichs@gmail.com>
2015-09-03 08:47:39 +0200

    Use templateHelper template rendering
    remove use of mustache and use the template hleper with handlebars

--- public/javascripts/content.js
+++ public/javascripts/content.js
@@ -58,9 +58,7 @@
       content.displayResults();
     },
     displayResults: function(){
-      matrix.template(content.el,
-                      'content-results',
-                      { pages: content.pages.slice(0,10) }
+      window.templateHelper.renderTemplate(content.el, 'content-results', { pages: content.pages.slice(0,10) }
       );
     },
     init: function(){
